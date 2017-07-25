5d7870e0c2602759f4a469d2decdf034283b2947
Himanshu Tamrakar <himanshu.tamrakar@yourmarch.com>
2017-07-25 23:06:59 +0530

    google login

--- server/social-config.js
+++ server/social-config.js
@@ -2,9 +2,19 @@ ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
 
+ServiceConfiguration.configurations.remove({
+    service: 'google'
+});
+
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
 
     appId: '668120650049837',
     secret: '8fe3d455e0e7a99ad4a54e35c4a1513d'
+});
+ServiceConfiguration.configurations.insert({
+    service: 'google',
+
+    clientId: '412632490346-oap9pkngua0khomn5ckvje1685fcnr6c.apps.googleusercontent.com',
+    secret: 'q9PsmIajlozNUl_FIhEp24qt'
 });
