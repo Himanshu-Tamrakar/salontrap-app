7f667ac68ad6405225e2468b3005131c0f01cc19
Himanshu Tamrakar <himanshu.tamrakar@yourmarch.com>
2017-10-28 01:33:22 +0530

    improvement

--- client/common/startup.js
+++ client/common/startup.js
@@ -1,9 +1,4 @@
 Meteor.startup(function() {
-  // Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://localhost:3000';
-  // process.env.ROOT_URL = "http://localhost:3000";
-  // Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://ec2-35-154-84-117.ap-south-1.compute.amazonaws.com';
-  // process.env.ROOT_URL = "http://ec2-35-154-84-117.ap-south-1.compute.amazonaws.com/";
-  Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://salontrap.com';
-  process.env.ROOT_URL = "http://salontrap.com/";
-
+  Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://localhost:3000';
+  process.env.ROOT_URL = "http://localhost:3000";
 });
--- server/social-config.js
+++ server/social-config.js
@@ -9,13 +9,13 @@ ServiceConfiguration.configurations.remove({
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
 
-    appId: '468465353496646',
-    secret: 'e8c4373587fc87d1454c576e50ee653d'
+    appId: 'appId',
+    secret: 'APP secret'
 });
 
 ServiceConfiguration.configurations.insert({
     service: 'google',
 
-    clientId: '412632490346-oap9pkngua0khomn5ckvje1685fcnr6c.apps.googleusercontent.com',
-    secret: 'q9PsmIajlozNUl_FIhEp24qt'
+    clientId: 'clientId',
+    secret: 'secret'
 });
